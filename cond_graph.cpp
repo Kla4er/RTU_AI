7a3e6fdb7bb7483f4daf729e256bb0eb26531955
Kla4er <andrejs.sohins@edu.rtu.lv>
2023-02-17 14:46:33 +0200

    FixBug
    Add fire var to struct

--- cond_graph.cpp
+++ cond_graph.cpp
@@ -7,12 +7,13 @@ struct TCondition{
   bool first;
   bool second;
   bool third;
+  bool fire;
   vector<int> child;
 };
 
 vector<TCondition> g;
 
-void FillGraph(int ptime, bool pfirst, bool psecond, bool pthird, int parrent) {
+void FillGraph(int ptime, bool pfirst, bool psecond, bool pthird, bool pfire, int parrent) {
   if (ptime < 0) return;
 
   TCondition current;
@@ -20,6 +21,7 @@ void FillGraph(int ptime, bool pfirst, bool psecond, bool pthird, int parrent) {
   current.first = pfirst;
   current.second = psecond;
   current.third = pthird;
+  current.fire = pfire;
 
   int curpos = g.size();
   if (parrent != -1) g[parrent].child.push_back(curpos);
@@ -27,23 +29,26 @@ void FillGraph(int ptime, bool pfirst, bool psecond, bool pthird, int parrent) {
 
   if (pfirst && psecond && pthird) return;
 
-  if (!pfirst && !psecond) FillGraph(ptime - 3, 1, 1, pthird, curpos);
-  if (!pfirst && !pthird)  FillGraph(ptime - 5, 1, psecond, 1, curpos);
-  if (!psecond && !pthird) FillGraph(ptime - 5, pfirst, 1, 1, curpos);
+  if (!pfirst && !psecond && !pfire) FillGraph(ptime - 3, 1, 1, pthird,!pfire, curpos);
+  if (!pfirst && !pthird && !pfire)  FillGraph(ptime - 5, 1, psecond, 1,!pfire, curpos);
+  if (!psecond && !pthird && !pfire) FillGraph(ptime - 5, pfirst, 1, 1,!pfire, curpos);
 
-  if (pfirst)  FillGraph(ptime - 1, 0, psecond, pthird, curpos);
-  if (psecond) FillGraph(ptime - 3, pfirst, 0, pthird, curpos);
-  if (pthird)  FillGraph(ptime - 5, pfirst, psecond, 0, curpos);
+  if (pfirst && pfire)  FillGraph(ptime - 1, 0, psecond, pthird,!pfire, curpos);
+  if (psecond && pfire) FillGraph(ptime - 3, pfirst, 0, pthird,!pfire, curpos);
+  if (pthird && pfire)  FillGraph(ptime - 5, pfirst, psecond, 0,!pfire, curpos);
 }
 
 void DrawGrahp(int v) {
   cout << "Versotne numurs " << v << endl;
   cout << " laiks = " << g[v].time << "  pirmais = " << g[v].first << " otrais = " << g[v].second << " tresais = " << g[v].third << endl;
-  cout << "Pecteci = { ";
-  for (auto u: g[v].child) {
-    cout << u << "; ";
+  if (g[v].child.size() > 0) {
+      cout << "Pecteci = { ";
+      for (auto u: g[v].child) {
+        cout << u << "; ";
+      }
+      cout << "}" << endl << endl;
   }
-  cout << "}" << endl << endl;
+  else cout << "Merka virsotne" << endl << endl;
 
   for (auto u: g[v].child) {
     DrawGrahp(u);
@@ -51,7 +56,7 @@ void DrawGrahp(int v) {
 }
 
 int main() {
-  FillGraph(12,0,0,0, -1);
+  FillGraph(12,0,0,0,0, -1);
   DrawGrahp(0);
   return 0;
 }
